300475e79b4ca6441ec87806e5e52200e51e67de
Tim Giblin <timmyg13@gmail.com>
2014-02-23 23:45:05 -0500

    nothing

--- mandrill.js
+++ mandrill.js
@@ -3,7 +3,7 @@ Meteor.Mandrill = {
     var host, key, port, username;
     username = options["username"];
     key = options["key"];
-    host = "smtp.mandrillapp.com";
+    host = "smtp.mandrillapp.com"; 
     port = "587";
     process.env.MAIL_URL = "smtp://" + username + ":" + key + "@"+ host + ":" + port + "/";
   },
