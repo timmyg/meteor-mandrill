516e92fef0eb2c08914453ec0b04603d1e8c684d
Basil Philipp <basil@philippundhee.ch>
2014-05-06 13:44:00 +0200

    Add support for merge tags.

--- mandrill.js
+++ mandrill.js
@@ -1,43 +1,48 @@
 Meteor.Mandrill = {
-  config: function(options) {
-    var host, key, port, username;
-    username = options["username"];
-    key = options["key"];
-    host = "smtp.mandrillapp.com"; 
-    port = "587";
-    process.env.MAIL_URL = "smtp://" + username + ":" + key + "@"+ host + ":" + port + "/";
-  },
-  send: function(options) {
-    Email.send(options);
-  },
-  sendTemplate: function(options) {
-    url = "https://mandrillapp.com/api/1.0/messages/send-template.json";
-    options = {
-      data: {
-        key: options.key,
-        template_name: options.templateSlug,
-        template_content: options.templateContent,
-        message: {
-          from_email: options.fromEmail,
-          to: [
-            {
-              email: options.toEmail
+    config: function (options) {
+        var host, key, port, username;
+        username = options["username"];
+        key = options["key"];
+        host = "smtp.mandrillapp.com";
+        port = "587";
+        process.env.MAIL_URL = "smtp://" + username + ":" + key + "@" + host + ":" + port + "/";
+    },
+    send: function (options) {
+        Email.send(options);
+    },
+    sendTemplate: function (options) {
+        var url = "https://mandrillapp.com/api/1.0/messages/send-template.json",
+            result;
+
+        options = {
+            data: {
+                key: options.key,
+                template_name: options.templateSlug,
+                template_content: options.templateContent,
+                message: {
+                    from_email: options.fromEmail,
+                    to: [
+                        {
+                            email: options.toEmail
+                        }
+                    ],
+                    "global_merge_vars": options.globalMergeVars,
+                    "merge_vars": options.mergeVars
+                },
+                headers: [
+                    {
+                        "Content-Type": "application/json"
+                    }
+                ]
             }
-          ]
-        },
-        headers: [
-          {
-            "Content-Type": "application/json"
-          }
-        ]
-      }
-    };
+        };
 
-    try {
-      result = HTTP.post(url, options);
-    } catch (e) {
-      console.log(e.stack);
+        try {
+            result = HTTP.post(url, options);
+
+        } catch (e) {
+            console.log(e.stack);
+        }
+
     }
-
-  }
 };
